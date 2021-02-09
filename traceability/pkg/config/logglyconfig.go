7813512ab578edfbc50c8950741e7964f597f8d5
sbolosan@axway.com <sbolosan@axway.com>
2021-02-09 14:15:18 -0700

    Update loggly Organization to Subdomain

--- traceability/pkg/config/logglyconfig.go
+++ traceability/pkg/config/logglyconfig.go
@@ -2,16 +2,16 @@ package config
 
 // LogglyConfig - represents the config for gateway
 type LogglyConfig struct {
-	Organization  string `config:"organization"`
+	Subdomain     string `config:"subdomain"`
 	CustomerToken string `config:"customerToken"`
 	APIToken      string `config:"apiToken"`
 	Host          string `config:"host"`
 	Port          string `config:"port"`
 }
 
-// GetOrganization - Returns the Loggly Organization
-func (l *LogglyConfig) GetOrganization() string {
-	return l.Organization
+// GetSubdomain - Returns the Loggly Organization
+func (l *LogglyConfig) GetSubdomain() string {
+	return l.Subdomain
 }
 
 // GetAPIToken - Returns the Loggly GetAPIToken
