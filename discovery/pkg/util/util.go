d59eb2ed0b9383ae94c4afdacdd17499fd4d0712
Jason Collins <jcollins@axway.com>
2021-12-13 06:56:36 -0700

    APIGOV-21043 - cleanup

--- discovery/pkg/util/util.go
+++ discovery/pkg/util/util.go
@@ -1,27 +1,8 @@
 package util
 
 import (
-	"archive/zip"
-	"fmt"
-	"io/ioutil"
-	"net/url"
 	"strconv"
 )
-
-// IsValidURL - tests a string to determine if it is a well-structured url or not.
-func IsValidURL(testString string) bool {
-	_, err := url.ParseRequestURI(testString)
-	if err != nil {
-		return false
-	}
-
-	u, err := url.Parse(testString)
-	if err != nil || u.Scheme == "" || u.Host == "" {
-		return false
-	}
-
-	return true
-}
 
 func ConvertStringToUint(val string) uint64 {
 	ret, _ := strconv.ParseUint(val, 10, 64)
@@ -30,17 +11,4 @@ func ConvertStringToUint(val string) uint64 {
 
 func ConvertUnitToString(val uint64) string {
 	return strconv.FormatUint(val, 10)
-}
-
-func FormatRemoteAPIID(proxyName, deployedEnvName, revisionName string) string {
-	return fmt.Sprintf("%v-%v-%v", proxyName, deployedEnvName, revisionName)
-}
-
-func ReadZipFile(zf *zip.File) ([]byte, error) {
-	f, err := zf.Open()
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	return ioutil.ReadAll(f)
 }
