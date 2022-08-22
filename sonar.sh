97a8dc6ec4e80109432eb911acf24aeb93b167f7
dfeldick <dfeldick@axway.com>
2022-08-22 13:14:41 -0700

    APIGOV-23407 - a

--- sonar.sh
+++ sonar.sh
@@ -8,8 +8,8 @@ sonar-scanner -X \
     -Dsonar.projectKey=Apigee_Agent_Client \
     -Dsonar.sourceEncoding=UTF-8 \
     -Dsonar.projectBaseDir=${WORKSPACE} \
-    -Dsonar.sources=./client/pkg \
-    -Dsonar.tests=. \
+    -Dsonar.sources=./client/pkg/...,./discovery/...,./traceability/... \
+    -Dsonar.tests=./client/pkg/...,./discovery/...,./traceability/... \
 	-Dsonar.exclusions=**/*.json \
     -Dsonar.test.inclusions=**/*test*.go \
     -Dsonar.go.tests.reportPaths=goreport.json \
