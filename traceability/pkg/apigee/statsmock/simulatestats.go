624ea74d7945f6fa1356eb6e0459f4b91550361e
Jason Collins <jcollins@axway.com>
2022-12-07 15:46:36 -0700

    APIGOV-24229 - add logging for trxn stats for stats mock

--- traceability/pkg/apigee/statsmock/simulatestats.go
+++ traceability/pkg/apigee/statsmock/simulatestats.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sync"
 	"time"
 
+	"github.com/Axway/agent-sdk/pkg/util/log"
 	"github.com/Axway/agents-apigee/client/pkg/apigee"
 	"github.com/Axway/agents-apigee/client/pkg/apigee/models"
 	"github.com/Axway/agents-apigee/traceability/pkg/apigee/definitions"
@@ -15,14 +17,20 @@ const maxNumAPIs = 5
 const maxNumTxns = 5
 
 type simulate struct {
-	client   *apigee.ApigeeClient
-	products apigee.Products
+	client        *apigee.ApigeeClient
+	products      apigee.Products
+	messageCounts map[string][]int
+	mapMutex      sync.Mutex
+	logger        log.FieldLogger
 }
 
 func NewStatsMock(client *apigee.ApigeeClient, products apigee.Products) definitions.StatsClient {
 	return &simulate{
-		client:   client,
-		products: products,
+		client:        client,
+		products:      products,
+		logger:        log.NewFieldLogger().WithComponent("simulate").WithPackage("statsmock"),
+		mapMutex:      sync.Mutex{},
+		messageCounts: make(map[string][]int),
 	}
 }
 
@@ -52,10 +60,31 @@ func (s *simulate) GetStats(env, dimension, metricSelect string, start, end time
 		response.Environments[0].Dimensions = append(response.Environments[0].Dimensions, s.genProductMetrics(p, start, end))
 	}
 
+	s.printStats()
+
 	return response, nil
 }
 
+func (s *simulate) printStats() {
+	s.mapMutex.Lock()
+	defer s.mapMutex.Unlock()
+	for product, stats := range s.messageCounts {
+		s.logger.
+			WithField("product", product).
+			WithField("total", stats[0]).
+			WithField("success", stats[1]).
+			WithField("policyErr", stats[2]).
+			WithField("serverErr", stats[3]).
+			Trace("*****stats*****")
+	}
+}
+
 func (s *simulate) genProductMetrics(productName string, start, end time.Time) models.MetricsDimensions {
+	s.mapMutex.Lock()
+	if _, ok := s.messageCounts[productName]; !ok {
+		s.messageCounts[productName] = []int{0, 0, 0, 0}
+	}
+	s.mapMutex.Unlock()
 	metric := models.MetricsDimensions{
 		Name: productName,
 		Metrics: []models.MetricsMetrics{
@@ -92,6 +121,13 @@ func (s *simulate) genProductMetrics(productName string, start, end time.Time) m
 			tErr = rand.Intn(total - pErr)
 		}
 		resTime := rand.Intn(100)
+		s.mapMutex.Lock()
+		s.messageCounts[productName][0] += total
+		s.messageCounts[productName][1] += (total - pErr - tErr)
+		s.messageCounts[productName][2] += pErr
+		s.messageCounts[productName][3] += tErr
+		s.mapMutex.Unlock()
+
 		metric.Metrics[0].MetricValues = append(metric.Metrics[0].MetricValues, models.MetricsValues{
 			Timestamp: timestamp,
 			Value:     fmt.Sprintf("%v.0", total),
