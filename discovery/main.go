97912a948376c4355db63d5b0192e866ec32e3c9
Jason Collins <jcollins@axway.com>
2024-02-09 11:30:57 -0700

    Revert "update sdk and add exception block"
    This reverts commit a3884b1082253e4b792dac150d1f28e2b86985a7.

--- discovery/main.go
+++ discovery/main.go
@@ -4,20 +4,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/Axway/agent-sdk/pkg/util/exception"
 	"github.com/Axway/agents-apigee/discovery/pkg/cmd"
 )
 
 func main() {
-	exception.Block{
-		Try: func() {
-			os.Setenv("AGENTFEATURES_VERSIONCHECKER", "false")
-			if err := cmd.RootCmd.Execute(); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		},
-		Catch: func(error) {},
-		Usage: "apigee.discovery.main",
-	}.Do()
+	os.Setenv("AGENTFEATURES_VERSIONCHECKER", "false")
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- traceability/main.go
+++ traceability/main.go
@@ -6,21 +6,14 @@ import (
 
 	// Required Import to setup factory for traceability transport (passivley include.  Do not remove _ (underscore))
 	_ "github.com/Axway/agent-sdk/pkg/traceability"
-	"github.com/Axway/agent-sdk/pkg/util/exception"
 
 	"github.com/Axway/agents-apigee/traceability/pkg/cmd"
 )
 
 func main() {
-	exception.Block{
-		Try: func() {
-			os.Setenv("AGENTFEATURES_VERSIONCHECKER", "false")
-			if err := cmd.RootCmd.Execute(); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		},
-		Catch: func(error) {},
-		Usage: "apigee.traceability.main",
-	}.Do()
+	os.Setenv("AGENTFEATURES_VERSIONCHECKER", "false")
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
