a3884b1082253e4b792dac150d1f28e2b86985a7
Jason Collins <jcollins@axway.com>
2024-02-09 11:23:02 -0700

    update sdk and add exception block

--- discovery/main.go
+++ discovery/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Axway/agent-sdk/pkg/util/exception"
 	"github.com/Axway/agents-apigee/discovery/pkg/cmd"
 )
 
 func main() {
-	os.Setenv("AGENTFEATURES_VERSIONCHECKER", "false")
-	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	exception.Block{
+		Try: func() {
+			os.Setenv("AGENTFEATURES_VERSIONCHECKER", "false")
+			if err := cmd.RootCmd.Execute(); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		},
+		Catch: func(error) {},
+		Usage: "apigee.discovery.main",
+	}.Do()
 }
